bcfabf4076b4bc274ddb6b975194be479dc0a7b0
MIKI <torandell9@hotmail.com>
2012-03-09 00:42:36 +0100

    Segón commit

---
+++ calculadora.java
@@ -0,0 +1,28 @@
+
+public class calculadora {
+
+	int num1;
+	
+	int num2;
+	
+	public calculadora(int num1, int num2){
+		
+		this.num1=num1;
+		
+		this.num2=num2;
+		
+	}
+	
+	
+	public int sumar(){
+		
+		return num1+num2;
+		
+	}
+	
+	public int multiplicar(){
+		
+		return num1*num2;
+		
+	}
+}
---
+++ impresora.java
@@ -0,0 +1,24 @@
+
+public class impresora {
+	
+	String cadena;
+	
+	public impresora(){
+	
+		super();
+	
+	}
+	
+	public void setCadena(String cadena){
+		
+		this.cadena=cadena;
+		
+	}
+	
+	public String getCadena(){
+		
+		return this.cadena;
+		
+	}
+
+}
